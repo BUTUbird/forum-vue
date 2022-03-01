78804ba5885c334a40ab4a47a2b74f6e3bd1ba23
BUTU <1781894948@qq.com>
2022-03-02 00:49:32 +0800

    推广链接

--- src/views/card/Promotion.vue
+++ src/views/card/Promotion.vue
@@ -4,23 +4,34 @@
       <span>推广</span>
     </div>
     <div>
-      body
+      <p v-for="(item,index) in list" :key="index" class="block">
+        <a :href="item.link" target="_blank">{{item.title}}</a>
+      </p>
     </div>
   </el-card>
 </template>
 
 <script>
+
+import {getList} from "@/api/promote";
+
 export default {
   name: "Promotion",
   data() {
     return{
-
+      list:[]
     }
   },
   created() {
+    this.fetchList();
   },
   methods:{
-
+    fetchList(){
+      getList().then((response)=>{
+        const {data} = response
+        this.list = data
+      })
+    }
   }
 }
 </script>
